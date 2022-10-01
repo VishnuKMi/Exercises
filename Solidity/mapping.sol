1e395939ddd35e59232b2654d5223445ce6721d7
VISHNU.KM <40514053+VishnuKMi@users.noreply.github.com>
2022-10-01 18:24:03 +0530

    Update mapping.sol

--- Solidity/mapping.sol
+++ Solidity/mapping.sol
@@ -1,24 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-contract Mapping {
+contract NestedMappings {
 
-    mapping (address => mapping(uint => bool)) public nestedMap;
+    // Mapping from address => (mapping from uint to bool)
+    mapping(address => mapping(uint => bool)) public nestedMap;
 
-    function get(address _addr, uint _i) public view returns (bool) {
-        return nestedMap[_addr][ _i];
+    function get(address _addr1, uint _i) public view returns (bool) {
+        // You can get values from a nested mapping
+        // even when it is not initialized
+        // The default value for a bool type is false
+        return nestedMap[_addr1][_i];
     }
 
-    function set (
-        address _addr,
+    function set(
+        address _addr1,
         uint _i,
         bool _boo
-        )
-        public {
-        nestedMap[_addr][_i] = _boo;
+    ) public {
+        nestedMap[_addr1][_i] = _boo;
     }
 
-    function remove(address _addr, uint _i) public {
-        delete myMap[_addr][_i];
+    function remove(address _addr1, uint _i) public {
+        delete nestedMap[_addr1][_i];
     }
 }

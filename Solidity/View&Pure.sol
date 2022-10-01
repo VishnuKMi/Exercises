6a5c5b94b52b3f8fcb648e4f66b4c5d23f739199
VISHNU.KM <40514053+VishnuKMi@users.noreply.github.com>
2022-10-01 18:22:10 +0530

    Update View&Pure.sol

--- Solidity/View&Pure.sol
+++ Solidity/View&Pure.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.10;
 
 contract ViewAndPure {
 
+    //declare a state variable
     uint public x = 1;
 
     //promise not to modify the state (but can read the state)
@@ -10,6 +11,7 @@ contract ViewAndPure {
         return x + y;
     }
 
+    //promise not to read or modify from state
     function add(uint i, uint j) public pure returns (uint) {
         return i + j;
     }

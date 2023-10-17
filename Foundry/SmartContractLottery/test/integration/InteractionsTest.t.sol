f60f0e9c298026d054450e490d7d806a95d816b4
VishnuKMi <vishnuarley@gmail.com>
2023-10-17 16:54:40 +0530

    Integration-tests setup added

---
+++ Foundry/SmartContractLottery/test/integration/InteractionsTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract InteractionsTest {
+    //TODO: Add tests for the following:
+    // 1. DeployRaffle.s.sol
+    // 2. Interactions.s.sol
+}
